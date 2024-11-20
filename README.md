91a54d350863f618ae9d9ad7dbd0ef0f65b3e3bd
SoftwareIISGubbio <software@iisgubbio.gov.it>
2024-11-20 15:11:26 +0100

    creata pagina più semplice

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # Tensione Corrente Potenza
 
 un semplice server per immagazzinare 3 misure e visualizzarle
+
+
+il server parte sulla porta 8008
+
+## query
+
+raggruppa le misure per mese
+
+SELECT FORMATDATETIME(TS,'yyyy-MM-dd') AS mese, count(*) from misura group by FORMATDATETIME(TS,'yyyy-MM-dd');
+
+cancellare un mese
+
+delete from misura where FORMATDATETIME(TS,'yyyy-MM')='2024-01';
+
+per compattare il file di H2
+
+SHUTDOWN COMPACT
+
+per testare velocità query
+
+SELECT misura.*
+FROM misura
+WHERE ts>DATEADD(HOUR, -1, CURRENT_TIMESTAMP())
+ORDER BY ts DESC LIMIT 20
--- src/main/java/it/edu/iisgubbio/tcp/MisuraRepository.java
+++ src/main/java/it/edu/iisgubbio/tcp/MisuraRepository.java
@@ -11,23 +11,23 @@ public interface MisuraRepository extends JpaRepository<Misura, Integer>{
 	@Query(value="SELECT misura.* "+
             "FROM misura "+
             "WHERE ts>DATEADD(HOUR, -1, CURRENT_TIMESTAMP()) "+
-            "ORDER BY ts DESC LIMIT 20", 
+            "ORDER BY ts DESC LIMIT 20",
        nativeQuery=true)
     List<Misura> ultimi20();
-    
+
     @Query(value="SELECT misura.* "+
             "FROM misura "+
-            "ORDER BY ts DESC LIMIT 1", 
+            "ORDER BY ts DESC LIMIT 1",
        nativeQuery=true)
     Misura ultimo();
-    
+
     @Query(value="SELECT misura.* "+
             "FROM misura "+
     		"WHERE fornitore=:quale "+
-            "ORDER BY ts DESC LIMIT 1", 
+            "ORDER BY ts DESC LIMIT 1",
        nativeQuery=true)
     Misura ultimo(@Param("quale") String quale);
-    
+
     @Query(value="SELECT HOUR (ts) as id, "+
     		"avg(corrente) corrente, "+
     		"avg(tensione) tensione, "+
@@ -40,32 +40,31 @@ public interface MisuraRepository extends JpaRepository<Misura, Integer>{
     		" GROUP BY HOUR (ts)",
        nativeQuery=true)
     List<Misura> giornata(@Param("data") String data);
-    
+
     @Query(value="SELECT 0 id, "+
-        "PERCENTILE_DISC(0.99999) WITHIN GROUP (ORDER BY corrente) as corrente, "+ //max non è significativo, becca anche dei numeri sballati
-        "PERCENTILE_DISC(0.99999) WITHIN GROUP (ORDER BY tensione) as tensione, "+
-        "PERCENTILE_DISC(0.99999) WITHIN GROUP (ORDER BY potenza) as potenza, "+
+        "PERCENTILE_DISC(0.97) WITHIN GROUP (ORDER BY corrente) as corrente, "+ //max non è significativo, becca anche dei numeri sballati
+        "PERCENTILE_DISC(0.97) WITHIN GROUP (ORDER BY tensione) as tensione, "+
+        "PERCENTILE_DISC(0.97) WITHIN GROUP (ORDER BY potenza) as potenza, "+
         "0 as rpm, "+
         "max(ts) as ts, "+
         "'stat' as fornitore"+
         " FROM misura",
        nativeQuery=true)
     Misura max(); // FIXME: servirebbe come parametro l'id del dispositivo
-    
+
     /*
      http://www.h2database.com/html/functions.html
-     
+
      select PERCENTILE_DISC(0.99999) WITHIN GROUP (ORDER BY corrente),
 PERCENTILE_DISC(0.99999) WITHIN GROUP (ORDER BY tensione),
 max(corrente)
  FROM misura
-     
-     
-SELECT  * from misura WHERE ts>DATEADD(SECOND, -1, CURRENT_TIMESTAMP()) 
-SELECT  sum(potenza) from misura WHERE ts>DATEADD(SECOND, -1, CURRENT_TIMESTAMP()) 
-     
-SELECT hour(ts), minute(ts),avg(potenza) 
-FROM misura 
+
+SELECT  * from misura WHERE ts>DATEADD(SECOND, -1, CURRENT_TIMESTAMP())
+SELECT  sum(potenza) from misura WHERE ts>DATEADD(SECOND, -1, CURRENT_TIMESTAMP())
+
+SELECT hour(ts), minute(ts),avg(potenza)
+FROM misura
 WHERE hour(ts)=6
 GROUP BY hour(ts), minute(ts)
 ORDER BY hour(ts), minute(ts) DESC
